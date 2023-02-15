79b5237e51e1d559a12605cbd7dccc416c9a3f4b
TechJerryyy <msohagia@aakashinformatics.com>
2023-02-15 16:56:39 +0530

    Add project files.

---
+++ cbasic5.5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace cbasic5._5
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter series of number saperated by Comma: ");
+            var input = Console.ReadLine();
+            var input1 = input.Split(',');
+            var max = Convert.ToInt32(input1[0]);
+            foreach (var str in input1)
+            {
+                var num = Convert.ToInt32(str);
+
+                if (num > max)
+                {
+                    max = num;
+                }
+
+            }
+            Console.WriteLine(max);
+
+        }
+    }
+}
